77538b16a5e9dfe36c49265a68b428b44afa67ee
Nelvinishere <161008445+Nelvinishere@users.noreply.github.com>
2024-07-05 21:11:32 +0000

    doing some updates

---
+++ Berryplant.js
@@ -0,0 +1,9 @@
+G.AddData({
+    name:'Berryplant mod',
+    author:'Nelvinishere',
+    desc:'Plant berries in an instant.',
+    engineVersion:1,
+    manifest:0,
+    requires:['Default dataset*'],
+    sheets:{'Berrysheet':'https://file.garden/ZZekSAtrsXoIMO7N/Mod%20Image/ImprovedBerry.png'}
+})
